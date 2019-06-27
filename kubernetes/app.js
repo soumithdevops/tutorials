b74b4e37a36e7034012e0f9266dd4471891c7de0
Gautam Borkar <31509355+gautam-borkar@users.noreply.github.com>
2019-06-27 15:51:27 -0400

    Update app.js

--- kubernetes/app.js
+++ kubernetes/app.js
@@ -3,8 +3,16 @@ const os = require('os');
 
 console.log("Kubia server starting...");
 
+var requestCount = 0;
+
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
+  requestCount++;
+  if (requestCount > 5) {
+    response.writeHead(500);
+    response.end("I'm not well. Please restart me!");
+    return;
+  }
   response.writeHead(200);
   response.end("You've hit " + os.hostname() + "\n");
 };
